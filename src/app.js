d00984719fa78adccde83c629e506228541fe64f
Jhon Nguyen <mrtuantrader@gmail.com>
2019-09-15 09:54:09 +0700

    [1.0.0] set up front

--- src/app.js
+++ src/app.js
@@ -1,21 +1,19 @@
 require('dotenv').config();
 import express from "express";
 import connectDB from "./config/connectDB";
-import userModel from "./models/user.model";
+import viewConfig from "./config/viewEngine";
 
 let app = express();
+//config database
 connectDB();
+//config view engine
+viewConfig(app);
 
-app.get('/', async (req, res)=>{
-    try {
-        let item = {
-            username: "tuan dep trai",   
-        }; 
-        let newUser = await userModel.createNew(item);   
-        res.send(`new user : ${newUser.username}`);
-    } catch (err) {
-        console.log(err);
-    }
+app.get('/', (req, res)=>{
+    res.render("index", {title: "homepage"});
+});
+app.get('/dashboard', (req, res)=>{
+    res.render("dashboard", { title: "dashboard"});
 });
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, ()=>{
