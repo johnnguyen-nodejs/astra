e67f4636316919d2e964b816314e29dbc502799a
Jhon Nguyen <mrtuantrader@gmail.com>
2019-09-15 12:24:34 +0700

    [1.0.0] config routes

--- src/app.js
+++ src/app.js
@@ -2,19 +2,15 @@ require('dotenv').config();
 import express from "express";
 import connectDB from "./config/connectDB";
 import viewConfig from "./config/viewEngine";
+import initRouter from "./routes/index";
 
 let app = express();
 //config database
 connectDB();
 //config view engine
 viewConfig(app);
-
-app.get('/', (req, res)=>{
-    res.render("index", {title: "homepage"});
-});
-app.get('/dashboard', (req, res)=>{
-    res.render("dashboard", { title: "dashboard"});
-});
+//config routes
+initRouter(app);
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, ()=>{
     console.log(` listening on port: ${process.env.APP_PORT}`);
