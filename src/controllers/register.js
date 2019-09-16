08b5eb8516151186eba338088a7516530db716a4
Jhon Nguyen <mrtuantrader@gmail.com>
2019-09-16 07:39:34 +0700

    [1.0.0] checkout

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -1,9 +1,7 @@
 import { validationResult } from "express-validator/check";
-import { auth } from "./../services/index";
 
-let register = async (req, res)=>{
+let register = (req, res)=>{
     let errArr = [];
-    let successArr = [];
     let validationErrors = validationResult(req);
     if(!validationErrors.isEmpty()){
         let errors = Object.values(validationErrors.mapped());
@@ -13,17 +11,6 @@ let register = async (req, res)=>{
         req.flash("errors", errArr)
         return res.redirect("/auth");  
     };
-    try {
-        let userCreateSuccess = await auth.register(req.body.email, req.body.password);
-        successArr.push(userCreateSuccess);
-        req.flash("success", successArr);
-        return res.redirect("/auth");
-    } catch (error) {
-        errArr.push(error);
-        req.flash("errors", errArr)
-        return res.redirect("/auth");
-    };
-    
 };
 
 module.exports = register;
